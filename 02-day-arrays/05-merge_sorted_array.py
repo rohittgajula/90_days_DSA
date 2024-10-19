bc86c6d3156b2880fad38f6235c23303628e3afe
Rohit Gajula <rohitgajula@MacBookAir.lan>
2024-10-19 10:14:09 +0530

    merge sort

---
+++ 02-day-arrays/05-merge_sorted_array.py
@@ -0,0 +1,30 @@
+
+
+def merge_sorted_array(nums1, m, nums2, n):
+    
+    i = m - 1           # pointer for stored part of nums1
+    j = n - 1           # pointer for nums2
+    k = m + n - 1       # pointer for actual len of nums1
+
+    while i >= 0 and j >= 0:
+        if nums1[i] > nums2[j]:
+            nums1[k] = nums1[i]
+            i -= 1
+        else:
+            nums1[k] = nums2[j]
+            j -= 1
+        k -= 1
+
+    while j >= 0:
+        nums1[k] = nums2[j]
+        j -= 1
+        k -= 1
+    
+    return nums1
+
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+print(merge_sorted_array(nums1, m, nums2, n))
